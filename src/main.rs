a5c236e1926c75f1bca2011b3df0ee2b584ac165
jtabalon <joseph@tabalon.io>
2022-07-31 08:24:12 -0700

    remove commented out sections

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,6 @@ use std::fs;
 fn parse_json(path: &str) -> Value {
     let data = fs::read_to_string(path).expect("Unable to read file");
     let res: serde_json::Value = serde_json::from_str(&data).expect("unable to parse json");
-    // println!("{}", res)
     res
 }
 
