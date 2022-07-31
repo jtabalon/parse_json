153e3a6b1a02d9d0138c963f3505cda5bc33e1f1
jtabalon <joseph@tabalon.io>
2022-07-31 08:23:48 -0700

    implement initial parse json function

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
 use serde_json;
+use serde_json::Value;
 use std::fs;
+
+fn parse_json(path: &str) -> Value {
+    let data = fs::read_to_string(path).expect("Unable to read file");
+    let res: serde_json::Value = serde_json::from_str(&data).expect("unable to parse json");
+    // println!("{}", res)
+    res
+}
 
 fn main() {
     let path = "./src/sample.json";
-    let data = fs::read_to_string(path).expect("Unable to read file");
-    let res: serde_json::Value = serde_json::from_str(&data).expect("unable to parse json");
-    println!("{}", res)
+    let result = parse_json(path);
+    println!("{:?}", result);
 }
