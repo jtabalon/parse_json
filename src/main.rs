d3eb484324ef90014dd40a3bfc371d7a23a6de10
jtabalon <joseph@tabalon.io>
2022-07-30 21:40:41 -0700

    add parse json statements

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,8 @@ use serde_json;
 use std::fs;
 
 fn main() {
-    println!("Hello, world!");
+    let path = "./src/sample.json";
+    let data = fs::read_to_string(path).expect("Unable to read file");
+    let res: serde_json::Value = serde_json::from_str(&data).expect("unable to parse json");
+    println!("{}", res)
 }
