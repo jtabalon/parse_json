35fdc141753c80f61f81a0295db44f9afd3303ee
Joseph Tabalon <jtabalon@sdsu.edu>
2022-08-06 10:38:48 -0700

    test write to file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use serde_json;
 use serde_json::Value;
 use std::fs;
+use std::fs::File;
 
 fn parse_json(path: &str) -> Value {
     let data = fs::read_to_string(path).expect("Unable to read file");
@@ -11,9 +12,17 @@ fn parse_json(path: &str) -> Value {
 fn main() {
     let path = "./src/sample.json";
     let result = parse_json(path);
-    println!("{:?}", result);
+
+    let mut f = File::create("foo.txt");
+
+    // fs::write("foo.txt", &result)
+
+    f.write_all(b"Hello, World!")
+
+
+    // println!("{:?}", result);
     // println!("{:?}", result[0].len());
-    println!("{:?}", result[0]["Country"]);
+    // println!("{:?}", result[0]["Country"]);
     // let country_result = result.clone();
     // let country_name: Value = country_result[0]["Country"];
     // println!("{}", country_name);
